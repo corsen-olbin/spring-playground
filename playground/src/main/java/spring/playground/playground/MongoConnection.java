9a2902bf8c0ccd26835475ac7e3a85920ecc7b36
corsen-olbin <corbinolsen97@gmail.com>
2019-02-17 20:50:31 -0600

    added in mongoDB example, calls this before starting spring service

---
+++ playground/src/main/java/spring/playground/playground/MongoConnection.java
@@ -0,0 +1,23 @@
+package spring.playground.playground;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+public class MongoConnection {
+    public static void PostToMongo() {
+        MongoClient mongoClient = new MongoClient();
+
+        MongoDatabase testdb = mongoClient.getDatabase("test");
+        MongoCollection<Document> testCollection = testdb.getCollection("test");
+
+        TestUser user = new TestUser("corbin", "password");
+        Document document = user.toDocument();
+        testCollection.insertOne(document);
+
+        mongoClient.close();
+    }
+}
---
+++ playground/src/main/java/spring/playground/playground/TestUser.java
@@ -0,0 +1,23 @@
+package spring.playground.playground;
+
+import org.bson.Document;
+
+/**
+ * TestUser
+ */
+public class TestUser extends Document{
+
+    public String User;
+    public String Password;
+
+    public TestUser(String user, String password){
+        User = user;
+        Password = password;
+    }
+
+    public Document toDocument() {
+        return new Document()
+        .append("user", User)
+        .append("password", Password);
+    }
+}
